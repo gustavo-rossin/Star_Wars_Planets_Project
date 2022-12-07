47a9cab14aa8e4a27e34bbb1bac65c98cc182fa9
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-12-07 14:20:06 -0300

    reqs 5 && 8 feito, testes 70%+ feitos

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,69 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
+import { clear } from '@testing-library/user-event/dist/clear';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import App from '../App';
+import Header from '../components/Header';
+import PlanetsProvider from '../context/PlanetsProvider';
+import PlanetsMock from './PlanetsMock';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Requisitos TESTES: 5, 8 e 10', () => {
+  beforeEach(() => {
+    jest.spyOn(global, "fetch");
+    global.fetch.mockResolvedValueOnce({
+      json: jest.fn().mockResolvedValue(PlanetsMock),
+    });
+    render(<App />)
+  })
+  test('1) Verificação do tamanho da tabela.', async () => {
+  const table = await screen.findAllByTestId('planet-name')
+  expect(table).toHaveLength(10);
 });
+
+test('2) Teste botões de filtro', async () => {
+    const name = screen.getByTestId("name-filter")
+    const column = screen.getByTestId("column-filter");
+    const comparison = screen.getByTestId("comparison-filter");
+    const value = screen.getByTestId("value-filter");
+    const button = await screen.findByTestId('button-filter');
+
+    expect(value).toHaveValue(0)
+    expect(comparison).toHaveValue('maior que')
+    expect(comparison).toHaveLength(3)
+    expect(column).toHaveValue('population')
+    expect(column).toHaveLength(5)
+    
+    userEvent.selectOptions(column, 'population');
+    userEvent.selectOptions(comparison, 'maior que');
+    userEvent.type(value, '1000');
+    userEvent.click(button);
+    userEvent.selectOptions(column, 'rotation_period');
+    userEvent.selectOptions(comparison, 'menor que');
+    userEvent.clear(value);
+    userEvent.type(value, '25');
+    userEvent.click(button);
+    userEvent.type(name,'oo');
+    userEvent.click(button);
+  });
+
+  test('3) verificação dos botões de ascender e descender.', async () => {
+    const ascendente = screen.getByRole('radio', {  name: /ascendente/i});
+    const ordenar = screen.getByRole('button', {  name: /ordenar/i})
+    const name = screen.getByTestId("name-filter")
+    const columnSort = screen.getByTestId("column-sort-button")
+    const dagobah = await screen.findByRole('cell', {  name: /Dagobah/i})
+    const alderaan = await screen.findByRole('cell', {  name: /alderaan/i})
+    const hoth = await screen.findByRole('cell', {  name: /hoth/i})
+    const yavin = await screen.findByRole('cell', {  name: /yavin iv/i})
+    
+    expect(dagobah).toBeInTheDocument();
+    userEvent.clear(name)
+    expect(alderaan).toBeInTheDocument();
+    userEvent.click(columnSort);
+    userEvent.click(ascendente);
+    expect(yavin).toBeInTheDocument();
+    expect(hoth).toBeInTheDocument();
+    userEvent.click(ordenar);
+
+  })
+})
